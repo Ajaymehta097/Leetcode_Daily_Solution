3f8c4b2975c2f9e25b54ef952204cb088da59204
Ajay Mehta <147687673+Ajaymehta097@users.noreply.github.com>
2026-07-17 08:16:09 +0530

    Time: 5 ms (47.24%), Space: 44.3 MB (25.28%) - LeetHub

---
+++ 2129-capitalize-the-title/2129-capitalize-the-title.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String capitalizeTitle(String title) {
+        StringBuilder sb = new StringBuilder();
+        String[] letters = title.split(" ");
+        int i=0;
+        for(String str:letters){
+            if(str.length() <= 2){
+                str = str.toLowerCase();
+                sb.append(str);
+                sb.append(" ");
+            }else{
+                str = str.toLowerCase();
+                str = Character.toUpperCase(str.charAt(0))+str.substring(1);
+                sb.append(str);
+                sb.append(" ");
+            }  
+        }
+        return sb.toString().trim();
+    }
+}
