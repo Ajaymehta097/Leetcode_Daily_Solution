c1da2af880d89528344d4fac0bdc5fe1d619433e
Ajay Mehta <147687673+Ajaymehta097@users.noreply.github.com>
2026-07-29 11:30:43 +0530

    Time: 5 ms (47.52%), Space: 45.2 MB (33.23%) - LeetHub

---
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isGood(int[] nums) {
+        Arrays.sort(nums);
+        int max = nums[nums.length-1];
+        if(nums.length != max + 1){
+            return false;
+        }
+        for (int i = 0; i < max - 1; i++) {
+            if (nums[i] != i + 1) {
+                return false;
+            }
+        }
+    return true;
+    }
+}
