5d21c8d26ec5824f324a5520400df64e92b2a98c
Ajay Mehta <147687673+Ajaymehta097@users.noreply.github.com>
2026-09-03 22:36:50 +0530

    Time: 3 ms (81.21%), Space: 43.6 MB (22.06%) - LeetHub

---
+++ 1496-path-crossing/1496-path-crossing.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isPathCrossing(String path) {
+        HashSet<String> set = new HashSet<>();
+        int x=0;
+        int y=0;
+        set.add(x+","+y);
+        for(char ch:path.toCharArray()){
+            if(ch == 'N'){
+                y++;
+            }
+            else if(ch == 'S'){
+                y--;
+            }
+            else if(ch == 'E'){
+                x++;
+            }
+            else if(ch == 'W'){
+                x--;
+            }
+            String position = x + "," + y;
+            if(set.contains(position)){
+                return true;
+            }
+            set.add(position);
+        }
+    return false;
+    }
+}
